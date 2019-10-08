e183036df30e32a6ba70f8241be66c2f41080a56
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-08 11:47:21 -0400

    Initial commit, remove storyboard

---
+++ xibs-and-programmatic-cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xibs-and-programmatic-cells
+//
+//  Created by David Rifkin on 10/8/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class DogsTableViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
